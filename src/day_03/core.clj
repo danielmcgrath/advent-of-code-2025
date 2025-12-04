7654c55cc93fff6b68aeb464e0fa5389497a06b2
Daniel McGrath <danielmcgrath@fastmail.com>
2025-12-03 21:43:25 -0500

    Add logic for Day 3 Part 2

--- src/day_03/core.clj
+++ src/day_03/core.clj
@@ -1,34 +1,44 @@
 (ns day-03.core
   (:require [clojure.string :as str]))
 
+(defn parse-line [line]
+  (map Long/parseLong (str/split line #"")))
+
 (defn parse [input]
   (->> (str/split-lines input)
        (remove str/blank?)
-       (map #(str/split % #""))
+       (map parse-line)
        vec))
 
-(defn pair-val [a b]
-  (Long/parseLong (str a b)))
+(defn find-best-index
+  [digits start last-start]
+  (loop [i      start
+         best-i start]
+    (if (> i last-start)
+      best-i
+      (if (pos? (compare (nth digits i) (nth digits best-i)))
+        (recur (inc i) i)
+        (recur (inc i) best-i)))))
 
-(defn line-max [digits]
-  (loop [[head & tail] digits
-         best Long/MIN_VALUE]
-    (if (nil? head)
-      best
-      (let [head-best (if (seq tail)
-                        (apply max best (map #(pair-val head %) tail))
-                        best)]
-        (recur tail head-best)))))
-
-(defn max-combos [lines]
-  (mapv line-max lines))
+(defn best-subseq
+  [digits k]
+  (let [n (count digits)]
+    (loop [start 0
+           k     k
+           acc   []]
+      (if (zero? k)
+        acc
+        (let [last-start (- n k)
+              j          (find-best-index digits start last-start)
+              acc'       (conj acc (nth digits j))]
+          (recur (inc j) (dec k) acc'))))))
 
 (defn v1 [input]
   (->> (parse input)
-       (max-combos)
+       (map #(Long/parseLong (apply str (best-subseq % 2))))
        (reduce +)))
 
 (defn v2 [input]
-  (let [nums (parse input)]
-    (apply max nums)))
-
+  (->> (parse input)
+       (map #(Long/parseLong (apply str (best-subseq % 12))))
+       (reduce +)))
