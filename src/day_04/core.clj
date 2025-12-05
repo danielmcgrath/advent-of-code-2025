f61c0185e0c4db8d5f8f26aa3ca7992523ae6bf9
Daniel McGrath <danielmcgrath@fastmail.com>
2025-12-04 22:54:22 -0500

    Add logic for Day 4 Part 2

--- src/day_04/core.clj
+++ src/day_04/core.clj
@@ -36,10 +36,61 @@
                     (< v 4))]
      pos)))
 
+(defn at-positions [grid]
+  (for [r (range (count grid))
+        c (range (count (first grid)))
+        :let [pos [r c]]
+        :when (= \@ (get-in grid pos))]
+    pos))
+
+(defn degree-map [grid]
+  (into {}
+        (for [pos (at-positions grid)]
+          [pos (neighboring-roll-count grid pos)])))
+
+(def empty-q clojure.lang.PersistentQueue/EMPTY)
+
+(defn max-clearable-rolls [grid]
+  (let [deg0 (degree-map grid)
+        ;; start with all '@' that are initially < 4
+        q0   (into empty-q
+                   (for [[pos d] deg0
+                         :when (< d 4)]
+                     pos))]
+    (loop [deg           deg0
+           cleared       #{}
+           q             q0
+           cleared-count 0]
+      (if (empty? q)
+        cleared-count
+        (let [pos (peek q)
+              q   (pop q)]
+          (if (cleared pos)
+            ;; already processed, skip
+            (recur deg cleared q cleared-count)
+            (let [cleared'       (conj cleared pos)
+                  cleared-count' (inc cleared-count)
+                  ;; update neighbors' degrees
+                  [deg' q']
+                  (reduce
+                   (fn [[deg q] npos]
+                     (if (and (= \@ (get-in grid npos))
+                              (not (cleared' npos)))
+                       (let [old (deg npos)
+                             new (dec old)
+                             deg (assoc deg npos new)
+                              ;; if this neighbor just dropped below 4, enqueue it
+                             q   (if (and (>= old 4) (< new 4))
+                                   (conj q npos)
+                                   q)]
+                         [deg q])
+                       [deg q]))
+                   [deg q]
+                   (neighbors grid pos))]
+              (recur deg' cleared' q' cleared-count'))))))))
+
 (defn v1 [input]
   (count-movable-rolls (parse input)))
 
 (defn v2 [input]
-  (let [nums (parse input)]
-    (apply max nums)))
-
+  (max-clearable-rolls (parse input)))
